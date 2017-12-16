848acf4c4763d9ffa5e395a7fdfe193cdf6e53eb
wedfvb <runescape@list.ru>
2017-12-16 21:36:34 +0300

    Added the main window - the one with account detals. A little rough still, but should do for now. I'll design it beautifully later.

---
+++ TeamProjectCKC/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeamProjectCKC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonQuestions_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonLogOut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonMatch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
