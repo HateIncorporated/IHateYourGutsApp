7834463198ad8f8aad4295bc99717fc2fcc11ba1
Егор Чернаков <evchernakov@edu.hse.ru>
2017-12-16 20:41:53 +0300

    Database classes fixes

---
+++ LibraryV1/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibraryV1
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        [InverseProperty("SenderUser")]
+        public virtual ICollection<Message> SenderMessage { get; set; }
+
+        [InverseProperty("RecieverUser")]
+        public virtual ICollection<Message> RecieverMessage { get; set; }
+    }
+}
