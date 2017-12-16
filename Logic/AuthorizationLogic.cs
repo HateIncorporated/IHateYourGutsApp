7dcafbe1076936901787d6e300363132dc9d4381
Георгий Корнеев <gakorneev@edu.hse.ru>
2017-12-16 21:53:29 +0300

    LoginCheking method

--- Logic/AuthorizationLogic.cs
+++ Logic/AuthorizationLogic.cs
@@ -9,7 +9,27 @@ namespace Logic
 {
     public class AuthorizationLogic
     {
+        public string LoginCheck;
+        public string PasswordCheck;
+        List<User> UsersAccounts = new List<User>();
 
+        public bool LoginCheking()
+        {
+            try
+            {
+                var User = UsersAccounts.Find(x => x.Login == LoginCheck);
+                if (User.Password == PasswordCheck)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
     }
 }
  
